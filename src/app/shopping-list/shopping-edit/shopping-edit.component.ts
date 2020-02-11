458b690769a751e80450acb1c7d8ad196eb3de62
Brandon <bm10@rawlingscompany.com>
2020-02-11 11:19:02 -0500

    edit singular component

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter,} from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('ingredientName', {static:false}) ingredientNameRef: ElementRef;
+  @ViewChild('ingredientAmount', {static:false}) ingredientAmountRef: ElementRef;
+  @Output() addIngredient = new EventEmitter<Ingredient>();
+
 
   constructor() { }
 
   ngOnInit() {
   }
 
-}
+  addNewIngredient(){
+     const ingredientName = this.ingredientNameRef.nativeElement.value;
+     const ingredientAmount = this.ingredientAmountRef.nativeElement.value;
+     const newIngredient = new Ingredient(ingredientName, ingredientAmount);
+
+     this.addIngredient.emit(newIngredient);
+
+
+  }
+} 
